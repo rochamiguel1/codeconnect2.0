f718f6f92875ef65702b52872881fb35511c54ac
rochamiguel1 <miroga2005@gmail.com>
2023-11-06 17:17:05 +0100

    centrar

--- src/components/MixAgency/MixEm.jsx
+++ src/components/MixAgency/MixEm.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 export default function MixEm({ text1, text2 }) {
     return (
-        <div className="contenedorem flex flex-col p-16 gap-5 bg-emfondo bg-opacity-50 bg-cover h-[600px]" >
+        <div className="contenedorem " >
             <h2 className=' justify-start w-[700px]  text-5xl text-blanco font-semibold' >{text1} </h2>
             <p className='  text-base pt-4 font-semibold text-blanco'> {text2}</p>
 
